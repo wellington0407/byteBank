609d622fce7a3547a0f02ed903d6c9a5759380cc
Wellington Francisco Benicio <wellington0407@gmail.com>
2021-05-14 09:28:12 -0300

    trabalhando com listas

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b2/d0df500a8cb2001b1e16b5ba1425c4c9
@@ -0,0 +1,7 @@
+
+//apenas para testar o tratamento de exceções
+public class Conta {
+
+    void deposita() {}
+
+}
---
+++ bytebank-herdado-conta/src/br/com/byteBank/banco/modelo/GuardadorDeContas.java
@@ -0,0 +1,25 @@
+package br.com.bytebank.banco.modelo;
+
+public class GuardadorDeContas {
+
+    private Object[] referencias;
+    private int posicaoLivre;
+
+    public GuardadorDeContas() {
+        this.referencias = new Conta[10];
+        this.posicaoLivre = 0;
+    }
+
+    public void adiciona(Conta ref) {
+        referencias[this.posicaoLivre] = ref;
+        this.posicaoLivre++;
+    }
+
+    public int getQuantidadeDeElementos() {
+        return this.posicaoLivre;
+    }
+
+    public Object getReferencia(int pos) {
+        return this.referencias[pos];
+    }
+}
